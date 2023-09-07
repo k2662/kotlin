f2b3f0814929cbeb927b69d6e84886a77b41185e
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2023-09-07 18:27:57 +0200

    K2: don't use FastJarFS if output jar is used in classpath (KT-61883)

--- compiler/cli/cli-base/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
+++ compiler/cli/cli-base/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
@@ -140,11 +140,27 @@ class KotlinCoreEnvironment private constructor(
                     val fastJarFs = applicationEnvironment.fastJarFileSystem
                     if (fastJarFs == null) {
                         messageCollector?.report(
-                            CompilerMessageSeverity.STRONG_WARNING,
+                            STRONG_WARNING,
                             "Your JDK doesn't seem to support mapped buffer unmapping, so the slower (old) version of JAR FS will be used"
                         )
                         applicationEnvironment.jarFileSystem
-                    } else fastJarFs
+                    } else {
+                        val outputJar = configuration.get(JVMConfigurationKeys.OUTPUT_JAR)
+                        if (outputJar == null) {
+                            fastJarFs
+                        } else {
+                            val contentRoots = configuration.get(CLIConfigurationKeys.CONTENT_ROOTS)
+                            if (contentRoots?.any { it is JvmClasspathRoot && it.file.path == outputJar.path } == true) {
+                                messageCollector?.report(
+                                    STRONG_WARNING,
+                                    "You reuse JAR from classpath ${outputJar.path} as output jar, so the slower (old) version of JAR FS will be used"
+                                )
+                                applicationEnvironment.jarFileSystem
+                            } else {
+                                fastJarFs
+                            }
+                        }
+                    }
                 }
 
                 else -> applicationEnvironment.jarFileSystem
