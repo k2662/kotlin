24f3c601f013c81a36039c2cf309ecbe9d8cf553
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2023-09-14 08:52:47 +0200

    fixup! LV 2.0: use bootstrapped Native bundle

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,7 +100,7 @@ IdeVersionConfigurator.setCurrentIde(project)
 
 if (!project.hasProperty("versions.kotlin-native")) {
     // BEWARE! Bumping this version doesn't take an immediate effect on TeamCity: KTI-1107
-    extra["versions.kotlin-native"] = "2.0-dev-2937"
+    extra["versions.kotlin-native"] = "1.9.30-dev-3194"
 }
 
 val irCompilerModules = arrayOf(
