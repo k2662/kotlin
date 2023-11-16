2eb6d73adb660b98e6520f07cc0bce3bf2aa601a
vladislav.grechko <vladislav.grechko@jetbrains.com>
2023-11-16 14:45:40 +0100

    fixup! Add JVM ABI K1/K2 consistency tests

--- libraries/tools/abi-comparator/build.gradle.kts
+++ libraries/tools/abi-comparator/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation(commonDependency("org.jetbrains.intellij.deps:asm-all"))
     implementation("org.apache.commons:commons-text:1.10.0")
     implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.7.0")
-    implementation("io.github.java-diff-utils:java-diff-utils:4.5")
+    implementation("io.github.java-diff-utils:java-diff-utils:4.12")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     implementation(project(":kotlin-stdlib-jdk8"))
